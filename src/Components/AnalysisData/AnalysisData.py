170b2493dd57da6eff13bbff909e5e9131b0a67b
xuexiaolei <xuexiaolei97@qq.com>
2024-08-08 09:08:29 +0800

    delete

--- src/Components/AnalysisData/AnalysisData.py
+++
@@ -1,7 +0,0 @@
-import os
-import chardet
-from PyQt5.QtWidgets import QFileDialog, QMessageBox, QWidget
-
-class AnalysisData(QWidget):
-    def __init__(self, file_path=""):
-        super().__init__()
